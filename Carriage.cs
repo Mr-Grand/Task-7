cd248e2de07b4a2bfaa611311cf92bb77ebc5983
Danik <mr.granddd@gmail.com>
2025-07-19 22:07:45 +0300

    После стиля

--- Carriage.cs
+++ Carriage.cs
@@ -2,14 +2,14 @@
 
 public class Carriage
 {
-    public string Name { get; private set; }
+    public string Name { get; private set; } = "Вагон №";
     public int Capacity { get; private set; }
     public int FreePositions { get; private set; }
     public Flight flight;
 
     public void SetCarriageName(string name)
     {
-        Name = "Вагон №" + name;
+        Name += name;
     }
 
     public void SetCapacity()
--- obj/Debug/net9.0/Task-7.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task-7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task-7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bfdfd8cf010dcd75c3d744d0f720c8d582ff14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b67e91d2b2bcdc1768b81f8f60f32a93bf6329")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task-7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task-7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
